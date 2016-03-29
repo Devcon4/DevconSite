ef595e08164c5f2e2b36ff6075f2eef112ad6409
Devyn Cyphers <devcon42@gmail.com>
2016-03-28 20:57:34 -0700

    First commit of the material design rework.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,15 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    
+    var getMainColor = function(){
+        var colors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green', 'light-green', 'lime', 'amber', 'orange', 'deep-orange', 'brown'];
+        return _.sample(colors);
     }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    
+    Session.set('MainColor', getMainColor());
+    
+    Template.registerHelper('mainColor', function () { 
+      return Session.get('MainColor'); 
+    });
 }
 
 if (Meteor.isServer) {
@@ -21,3 +17,52 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+/* Routes and router config for DevconSite. */
+
+/* Landing page. */
+Router.route('/', {
+  name: 'home',
+  template: 'home'
+});
+
+/* Admin panel login */
+Router.route('/login', {
+  name: 'login',
+  tmeplate: 'login'
+});
+
+/* Admin panel */
+Router.route('/admin', {
+  name: 'admin',
+  tmeplate: 'admin'
+});
+
+/* Resume checklist. */
+Router.route('/resume', {
+  name: 'resume',
+  tmeplate: 'resume'
+});
+
+/* Projects page. */
+Router.route('/projects', {
+  name: 'projects',
+  template: 'projects'
+});
+
+/* Games page */
+Router.route('/games', {
+  name: 'games',
+  template: 'games'
+});
+
+/* Github page */
+Router.route('/github', {
+  name: 'github',
+  template: 'github'
+});
+
+/* Sets the layout Template to Layout1. */
+Router.configure({
+  layoutTemplate: 'layout1'
+});
